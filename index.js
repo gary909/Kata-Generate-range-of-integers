85d09493dfb1699d0245b519ef4e12481b82a342
gary <gary.white@lodgit.com>
2022-02-10 14:26:20 +0100

    code working

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 function generateRange(min, max, step){
-
+    let myArr = [];
+    let myNum = step;
+    for (let i = min; i <= max; i= i+myNum) {
+        myArr.push(i)
+    }
+    return myArr;
 }
 
 console.log(generateRange(2, 10, 2)); // return [2,4,6,8,10]
